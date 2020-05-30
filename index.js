cba108839841e4c60a951551486190cd66e1acce
nnmrts <nanomiratus@gmail.com>
2020-05-30 06:48:25 +0200

    ??????????
    how the hell did i not notice that? what am i even doing? WHAT???

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ module.exports = {
 	},
 	extends: [
 		"./rules/best-practices.js",
+		"./rules/compat.js",
 		"./rules/errors.js",
 		"./rules/es6.js",
+		"./rules/flow.js",
 		"./rules/imports.js",
+		"./rules/jsdoc.js",
 		"./rules/node.js",
 		"./rules/strict.js",
 		"./rules/style.js",
